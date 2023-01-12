408fd31b77c85942eb6dc1894ba0b88782428e11
Bob Merullo <bmerullo85@gmail.com>
2023-01-11 21:10:43 -0700

    ISR changes

--- next-routing/pages/404.js
+++
@@ -1,5 +0,0 @@
-const PageNotFound = () => {
-  return <hh1>404 Page with all the custom styling necessary</hh1>;
-};
-
-export default PageNotFound;
--- next-routing/pages/index.js
+++ next-routing/pages/index.js
@@ -1,25 +1,5 @@
-import Link from "next/link";
-import { useRouter } from "next/router";
-
 const Home = () => {
-  const router = useRouter();
-
-  const handleClick = () => {
-    console.log("Placing your order");
-    router.push("/product");
-  };
-  return (
-    <div>
-      <h1>Home Page</h1>
-      <Link href="/blog">
-        <p>Blog</p>
-      </Link>
-      <Link href="/product">
-        <p>Products</p>
-      </Link>
-      <button onClick={handleClick}>Place Order</button>
-    </div>
-  );
+  return <h1>Home Page</h1>;
 };
 
 export default Home;
