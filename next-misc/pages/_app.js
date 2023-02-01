96c50c2a5d1b50a2b6f75ef673fd1a4a8ade15c2
Bob Merullo <bmerullo85@gmail.com>
2023-02-01 16:52:05 -0700

    App Layout

---
+++ next-misc/pages/_app.js
@@ -0,0 +1,17 @@
+import Header from "@/components/Header";
+import Footer from "@/components/Footer";
+import "@/styles/globals.css";
+import "../styles/layout.css";
+
+export default function App({ Component, pageProps }) {
+  if (Component.getLayout) {
+    return Component.getLayout(<Component {...pageProps} />);
+  }
+  return (
+    <>
+      <Header />
+      <Component {...pageProps} />
+      <Footer />
+    </>
+  );
+}
