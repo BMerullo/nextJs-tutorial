1bd76ec9e06610f939f2c4ee04ba07f9b4d45199
Bob Merullo <bmerullo85@gmail.com>
2023-02-14 12:18:21 -0700

    created next-auth2

--- next-auth/components/Navbar.js
+++ next-auth/components/Navbar.js
@@ -1,58 +1,31 @@
 import Link from "next/link";
-import { signIn, signOut, useSession } from "next-auth/client";
+// import { signIn, signOut, useSession } from "next-auth/client";
 
 function Navbar() {
-  const [session, loading] = useSession();
+  // const [session, loading] = useSession();
   return (
     <nav className="header">
       <h1 className="logo">
         <a href="#">NextAuth</a>
       </h1>
-      <ul className={`main-nav ${!session && loading ? "loading" : "loaded"}`}>
+      <ul className={`main-nav `}>
         <li>
-          <Link href="/">
-            <a>Home</a>
-          </Link>
+          <Link href="/">Home</Link>
         </li>
         <li>
-          <Link href="/dashboard">
-            <a>Dashboard</a>
-          </Link>
+          <Link href="/dashboard">Dashboard</Link>
         </li>
         <li>
-          <Link href="/blog">
-            <a>Blog</a>
-          </Link>
+          <Link href="/blog">Blog</Link>
         </li>
 
-        {!loading && !session && (
-          <li>
-            <Link href="/api/auth/signin">
-              <a
-                onClick={(e) => {
-                  e.preventDefault();
-                  signIn("github");
-                }}
-              >
-                Sign In
-              </a>
-            </Link>
-          </li>
-        )}
-        {session && (
-          <li>
-            <Link href="/api/auth/signout">
-              <a
-                onClick={(e) => {
-                  e.preventDefault();
-                  signOut();
-                }}
-              >
-                Sign Out
-              </a>
-            </Link>
-          </li>
-        )}
+        <li>
+          <Link href="#">Sign In</Link>
+        </li>
+
+        <li>
+          <Link href="#">Sign Out</Link>
+        </li>
       </ul>
     </nav>
   );
