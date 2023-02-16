862508868417baa5f7e1ba36d300de6903da0658
Bob Merullo <bmerullo85@gmail.com>
2023-02-15 18:35:22 -0700

    Client-side Authentication

--- next-auth/components/Navbar.js
+++ next-auth/components/Navbar.js
@@ -1,8 +1,10 @@
 import Link from "next/link";
-import { signIn, signOut } from "next-auth/react";
+import { signIn, signOut, useSession } from "next-auth/react";
 
 function Navbar() {
-  // const [session, loading] = useSession();
+  const { data: session } = useSession();
+  console.log({ session });
+
   return (
     <nav className="header">
       <h1 className="logo">
@@ -18,26 +20,29 @@ function Navbar() {
         <li>
           <Link href="/blog">Blog</Link>
         </li>
-        <li>
-          <Link
-            href="/api/auth/signin"
-            onClick={(e) => {
-              e.preventDefault(), signIn();
-            }}
-          >
-            Sign In
-          </Link>
-        </li>
-        <li>
-          <Link
-            href="/api/auth/signout"
-            onClick={(e) => {
-              e.preventDefault(), signOut();
-            }}
-          >
-            Sign Out
-          </Link>
-        </li>
+        {session ? (
+          <li>
+            <Link
+              href="/api/auth/signout"
+              onClick={(e) => {
+                e.preventDefault(), signOut();
+              }}
+            >
+              Sign Out
+            </Link>
+          </li>
+        ) : (
+          <li>
+            <Link
+              href="/api/auth/signin"
+              onClick={(e) => {
+                e.preventDefault(), signIn();
+              }}
+            >
+              Sign In
+            </Link>
+          </li>
+        )}
       </ul>
     </nav>
   );
