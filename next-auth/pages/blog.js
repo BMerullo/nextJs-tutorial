e3eac3047220f3fb43fd88b8361428742fc249ba
Bob Merullo <bmerullo85@gmail.com>
2023-02-17 12:19:41 -0700

    Server-side Authentication

--- next-auth/pages/blog.js
+++ next-auth/pages/blog.js
@@ -1,17 +1,22 @@
 import React from "react"
-import { getSession } from "next-auth/react"
+import { getSession, useSession } from "next-auth/react"
 
-const Blog = () => {
-  return <h1>Blog page</h1>
+const Blog = ({ data }) => {
+  const { data: session, status } = useSession()
+  console.log({ session, status })
+  return <h1>Blog page - {data}</h1>
 }
 
 export default Blog
 
 export async function getServerSideProps(context) {
-  const session = getSession(context)
+  const session = await getSession(context)
   return {
     props: {
-      data: session ? "List of 100 personalised blogs" : "List of free blogs",
+      session,
+      data: session
+        ? `List of 100 personalised blogs for ${session.user.name}`
+        : "List of free blogs",
     },
   }
 }
